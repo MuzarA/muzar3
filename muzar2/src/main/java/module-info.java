942a9539b17f06577fd5a1340b02954ca186793d
Muzar <muzar5374@gmail.com>
2024-12-22 17:12:03 +0600

    Delete muzar2 directory

--- muzar2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.employeedatabasemanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.employeedatabasemanagement to javafx.fxml;
-    exports org.example.employeedatabasemanagement;
-}
